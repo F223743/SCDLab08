7ea39cdee9c44a95d81c28fab44beb8ce1989b6f
F223743 <f223743@cfd.nu.edu.pk>
2024-10-10 10:27:07 +0500

    Updated Button desgin and font size

---
+++ src/designUI.java
@@ -0,0 +1,6 @@
+public void designUI() {
+    // Setting button color to green
+    button.setBackground(Color.GREEN);
+    // Setting font size for better visibility
+    button.setFont(new Font("Arial", Font.PLAIN, 16));
+}
